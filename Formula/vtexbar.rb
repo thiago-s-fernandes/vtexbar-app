6fe2e6fbf43b8dfd4bd8c102c4b27ebc7d4a68f7
Thiago Fernandes <thiago.souzax3@outlook.com>
2026-09-24 18:39:08 -0300

    chore(release): update vtexbar Homebrew Cask checksum for modular release

--- Formula/vtexbar.rb
+++ Formula/vtexbar.rb
@@ -1,6 +1,6 @@
 cask "vtexbar" do
   version "1.0.0"
-  sha256 "db34d0e37d49ed26f2a3e2db456326eae559e95b6f8cdd47bd1441962aa2ac15"
+  sha256 "41d90b09ca95e31da211d76d9aa4d29594cea4280267fa4f191aed963e14529b"
 
   url "https://github.com/thiago-s-fernandes/vtexbar-app/releases/download/v#{version}/VTEX-Bar.dmg"
   name "VTEX Bar"
