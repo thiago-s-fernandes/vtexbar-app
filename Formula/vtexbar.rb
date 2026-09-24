80af0262535ef24a19b135eb9ab515ec779086b5
Thiago Fernandes <thiago.souzax3@outlook.com>
2026-09-24 12:08:50 -0300

    chore(formula): update vtexbar sha256 checksum

--- Formula/vtexbar.rb
+++ Formula/vtexbar.rb
@@ -1,6 +1,6 @@
 cask "vtexbar" do
   version "1.0.0"
-  sha256 "fc9f9545ee6a2d8a35fe84e3cca0a875d46447b72157cbd4a6c7c660ddb023cf"
+  sha256 "e7c0c0c4ec79abd3c6702b117e1202af3126d33185c4930d03f29493b1df5dd0"
 
   url "https://github.com/thiago-s-fernandes/vtexbar-app/releases/download/v#{version}/VTEX-Bar.dmg"
   name "VTEX Bar"
