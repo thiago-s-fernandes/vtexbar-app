41a321f6909cc1a8fbacbe69677a16bdcb1bd9ee
Thiago Fernandes <thiago.souzax3@outlook.com>
2026-09-23 22:01:17 -0300

    chore: update vtexbar formula sha256 for spotlight search release

--- Formula/vtexbar.rb
+++ Formula/vtexbar.rb
@@ -1,6 +1,6 @@
 cask "vtexbar" do
   version "1.0.0"
-  sha256 "fa714b62b959052ed5d6cd6df1aafda718f64402a9206c444d4fb9066d5f66a3"
+  sha256 "9930537f15d600ec8d946b3fe010124fa74361fd702504886633640d926f052b"
 
   url "https://github.com/thiago-s-fernandes/vtexbar-app/releases/download/v#{version}/VTEX-Bar.dmg"
   name "VTEX Bar"
